6e3a9778aab8dd77068d56a1d7b98d21ca63f762
tlkhanh <longkhanhta@gmail.com>
2024-09-08 22:39:46 +0700

    feat: add simple console c sharp application

--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -1,11 +1,27 @@
-﻿using System;
-namespace SimpleCSharpApp
+﻿// Display a message and wait for Enter key to be pressed.
+Console.WriteLine("***** My First C# App *****");
+Console.WriteLine("Hello World!");
+Console.WriteLine();
+// Process any incoming args.
+for (int i = 0; i < args.Length; i++)
 {
-    class Program
+    Console.WriteLine("Arg: {0}", args[i]);
+}
+
+Console.Beep();
+// Return an arbitrary error code.
+ShowEnvironmentDetails();
+return 0;
+
+static void ShowEnvironmentDetails()
+{
+    // Print out the drives on this machine,
+    // and other interesting details.
+    foreach (string drive in Environment.GetLogicalDrives())
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Drive: {0}", drive);
     }
+    Console.WriteLine("OS: {0}", Environment.OSVersion);
+    Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+    Console.WriteLine(".NET Core Version: {0}", Environment.Version);
 }
