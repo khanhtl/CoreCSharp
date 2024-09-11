676854de673a1a7e6cc119a46456115aec7352d0
tlkhanh <longkhanhta@gmail.com>
2024-09-11 23:45:17 +0700

    feat: enum

---
+++ Enum/Program.cs
@@ -0,0 +1,83 @@
+﻿Console.WriteLine("**** Fun with Enums *****");
+// Make an EmpTypeEnum variable.
+EmpTypeEnum emp = EmpTypeEnum.Contractor;
+AskForBonus(emp);
+Console.WriteLine("EmpTypeEnum uses a {0} for storage", Enum.GetUnderlyingType(typeof(EmpTypeEnum)));
+Console.WriteLine("{0} = {1}", emp.ToString(), (byte)emp);
+
+// Prints out "emp is a Contractor".
+Console.WriteLine("emp is a {0}.", emp.ToString());
+EvaluateEnum(emp);
+Console.ReadLine();
+
+Console.WriteLine("===== Fun wih Bitwise Operations");
+Console.WriteLine("6 & 4 = {0} | {1}", 6 & 4, Convert.ToString((6 & 4), 2));
+Console.WriteLine("6 = {0} | {1}", 6, Convert.ToString((6), 2));
+Console.WriteLine("6 | 4 = {0} | {1}", 6 | 4, Convert.ToString((6 | 4), 2));
+Console.WriteLine("6 ^ 4 = {0} | {1}", 6 ^ 4, Convert.ToString((6 ^ 4), 2));
+Console.WriteLine("6 << 1 = {0} | {1}", 6 << 1, Convert.ToString((6 << 1), 2));
+Console.WriteLine("6 >> 1 = {0} | {1}", 6 >> 1, Convert.ToString((6 >> 1), 2));
+Console.WriteLine("~6 = {0} | {1}", ~6, Convert.ToString(~(6), 2));
+Console.WriteLine("Int.MaxValue {0}", Convert.ToString((int.MaxValue), 2));
+Console.ReadLine();
+
+ContactPreferenceEnum emailAndPhone = ContactPreferenceEnum.Email | ContactPreferenceEnum.Phone;
+Console.WriteLine("None? {0}", (emailAndPhone | ContactPreferenceEnum.None) == emailAndPhone);
+Console.WriteLine("Email? {0}", (emailAndPhone | ContactPreferenceEnum.Email) == emailAndPhone);
+Console.WriteLine("Phone? {0}", (emailAndPhone | ContactPreferenceEnum.Phone) == emailAndPhone);
+Console.WriteLine("Text? {0}", (emailAndPhone | ContactPreferenceEnum.Text) == emailAndPhone);
+// Enums as parameters.
+static void AskForBonus(EmpTypeEnum e)
+{
+    switch (e)
+    {
+        case EmpTypeEnum.Manager:
+            Console.WriteLine("How about stock options instead?");
+            break;
+        case EmpTypeEnum.Grunt:
+            Console.WriteLine("You have got to be kidding...");
+            break;
+        case EmpTypeEnum.Contractor:
+            Console.WriteLine("You already get enough cash...");
+            break;
+        case EmpTypeEnum.VicePresident:
+            Console.WriteLine("VERY GOOD, Sir!");
+            break;
+    }
+}
+
+// This method will print out the details of any enum.
+static void EvaluateEnum(System.Enum e)
+{
+    Console.WriteLine("=> Information about {0}", e.GetType().Name);
+    Console.WriteLine("Underlying storage type: {0}",
+    Enum.GetUnderlyingType(e.GetType()));
+    // Get all name-value pairs for incoming parameter.
+    Array enumData = Enum.GetValues(e.GetType());
+    Console.WriteLine("This enum has {0} members.", enumData.Length);
+    // Now show the string name and associated value, using the D format
+    // flag (see Chapter 3).
+    for (int i = 0; i < enumData.Length; i++)
+    {
+        Console.WriteLine("Name: {0}, Value: {0:D}", enumData.GetValue(i));
+    }
+}
+
+
+enum EmpTypeEnum
+{
+    Manager, // = 0
+    Grunt, // = 1
+    Contractor, // = 2
+    VicePresident // = 3
+}
+
+[Flags]
+public enum ContactPreferenceEnum
+{
+    None = 1,
+    Email = 2,
+    Phone = 4,
+    Ponyexpress = 6,
+    Text = 7
+}
